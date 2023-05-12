61f56529120bd067629c853b4ab224e96e296834
Austinps <aptstack@gmail.com>
2023-05-12 07:03:24 +0200

    update env URls

--- server.js
+++ server.js
@@ -5,8 +5,9 @@ dotenv.config();
 
 const isProduction = process.env.NODE_ENV === 'production';
 const PORT = isProduction ? process.env.PORT : 3000;
+const URL = isProduction ? process.env.PROD_URL : process.env.DEV_URL;
 
 app.listen(PORT, () => {
   // Open in your browser
-  console.log(`listen on http://127.0.0.1:${PORT}`);
+  console.log(`listening on ${URL}:${PORT}`);
 });
--- src/routes/viewRouter.js
+++ src/routes/viewRouter.js
@@ -2,7 +2,7 @@
 
 import { Router } from 'express';
 import cookieParser from 'cookie-parser';
-import { doubleCsrf } from 'csrf-csrf';
+
 import {
   renderHome,
   renderProtected,
@@ -10,10 +10,8 @@ import {
 } from '../controllers/viewControllers.js';
 import config from '../../config/config.js';
 
-
-
-
 const router = Router();
+
 router.use(cookieParser(config.cookiesSecret, { signed: true }));
 router.get('/', renderHome);
 router.get('/vulnerable', renderVulnerable);
